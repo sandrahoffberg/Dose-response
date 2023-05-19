0a0c4937eea66343775c4a14df3b30a9973404ff
June Snedecor <june@codeocean.com>
2023-05-19 21:21:15 +0000

    Edited file.sh

--- code/file.sh
+++ code/file.sh
@@ -3,4 +3,6 @@ set -ex
 
 source ./config.sh
 
-python ./run_dose_response.py --metadata_file ${metadata} --all_rpm_file ${all_rpm} --target_list ${target_list}
+echo python ./run_dose_response.py --metadata_file ${metadata} --all_rpm_file ${all_rpm} --target_list ${target_list}
+
+exit 1
